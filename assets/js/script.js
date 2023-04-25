28dfdbd29c185b61eb0305eb56bf74508954a4dc
AnBarr45 <bpena2063@e-camp.cl>
2023-04-25 18:01:11 -0400

    html edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,7 @@ function addDigimon(data, idx) {
                   ${data.name} 
               
                   </a>
+                <img src"${data.img}">
                   
             </td>
         </tr> 
